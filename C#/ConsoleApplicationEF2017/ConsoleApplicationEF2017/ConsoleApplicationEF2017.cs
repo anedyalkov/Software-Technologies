35b1c832c61dcc8e6f0510b87f2677da4f7fda68
anedyalkov <nedyalkov78@gmail.com>
2017-08-14 21:29:35 +0300

    Added new file

---
+++ C#/ConsoleApplicationEF2017/ConsoleApplicationEF2017/ConsoleApplicationEF2017.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplicationEF2017
+{
+    class ConsoleApplicationEF2017
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+}
