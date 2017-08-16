63c2696d0ac2589897398b7acc0ef40ed92911ad
anedyalkov <nedyalkov78@gmail.com>
2017-08-16 23:04:21 +0300

    Added BookLibrary project

---
+++ C#/BookLibrary/BookLibrary/Controllers/BookController.cs
@@ -0,0 +1,109 @@
+﻿using System.Data.Entity;
+using BookLibrary.Models;
+using System.Linq;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+
+namespace BookLibrary.Controllers
+{
+    public class BookController : Controller
+    {
+        // GET: Book
+        public ActionResult Index()
+        {
+            using (var db = new ApplicationDbContext())
+            {
+                var books = db.Books.Include(b=>b.Author).ToList();
+
+                return View(books);
+            }
+        }
+
+        // GET: Book/Details/5
+        public ActionResult Details(int id)
+        {
+            using (var db = new ApplicationDbContext())
+            {
+                var book = db.Books.Include(b => b.Author).SingleOrDefault(b => b.Id == id);
+
+                if (book == null)
+                {
+                    return new HttpNotFoundResult($"No book found. Please check the book id.");
+                }
+
+                return View(book);
+            }
+        }
+
+        // GET: Book/Create
+        [Authorize]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Book/Create
+        [HttpPost]
+        [Authorize]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Book book)
+        {
+            using (var db = new ApplicationDbContext())
+            {
+                var userId = User.Identity.GetUserId();
+
+                book.AuthorId = userId;
+
+                db.Books.Add(book);
+
+                db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+        }
+
+        // GET: Book/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Book/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Book/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Book/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
