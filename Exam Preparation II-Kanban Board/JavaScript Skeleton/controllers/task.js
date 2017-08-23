d6bb9ba9bd1258c636db91de8f047703f2a56321
anedyalkov <nedyalkov78@gmail.com>
2017-08-23 17:12:27 +0300

    Added TeisterMask-JavaScript

--- Exam Preparation II-Kanban Board/JavaScript Skeleton/controllers/task.js
+++ Exam Preparation II-Kanban Board/JavaScript Skeleton/controllers/task.js
@@ -3,17 +3,53 @@ const Task = require('../models/Task');
 module.exports = {
 	index: (req, res) => {
 		// TODO: Implement me...
-	},
+        Task.find().then(tasks => {
+            res.render('task/index', {
+                'openTasks': tasks.filter(t => t.status === "Open"),
+                'inProgressTasks': tasks.filter(t => t.status === 'In Progress'),
+                'finishedTasks': tasks.filter(t => t.status === 'Finished')
+            });
+        });
+
+    },
 	createGet: (req, res) => {
 		// TODO: Implement me...
+        res.render('task/create');
 	},
 	createPost: (req, res) => {
 		// TODO: Implement me...
+        let task = req.body;
+
+        Task.create(task).then(task => {
+            res.redirect('/');
+        }).catch(err => {
+            task.error = "Cannot create task.";
+            return res.render('task/create', task);
+        });
 	},
 	editGet: (req, res) => {
 		// TODO: Implement me...
+        let id = req.params.id;
+
+        Task.findById(id).then(task => {
+            if (task) {
+                return res.render('task/edit', task);
+            } else {
+                return res.redirect('/');
+            }
+        }).catch(err => res.redirect('/'));
 	},
 	editPost: (req, res) => {
 		// TODO: Implement me...
+        let id = req.params.id;
+        let task = req.body;
+
+        Task.findByIdAndUpdate(id, task, {runValidators: true}).then(task => {
+            res.redirect('/');
+        }).catch(err => {
+            task.id = id;
+            task.error = "Cannot edit task.";
+            return res.render('task/edit', task);
+        });
 	}
 };
--- Exam Preparation II-Kanban Board/JavaScript Skeleton/models/Task.js
+++ Exam Preparation II-Kanban Board/JavaScript Skeleton/models/Task.js
@@ -2,8 +2,10 @@ const mongoose = require('mongoose');
 
 let taskSchema = mongoose.Schema({
 	// TODO: Implement me...
+    title: {type: 'string', required: true},
+    status: {type: 'string', required: true}
 });
 
-let Task = mongoose.model('Task', taskSchema);
+let Task = mongoose.model('Task', taskSchema,'allTasks');
 
 module.exports = Task;
