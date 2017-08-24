c0467fdc6f4642a26f98f4c8f82550c9dd0b6ecb
anedyalkov <nedyalkov78@gmail.com>
2017-08-24 18:54:29 +0300

    Added Exam Preparation III-IMDB

---
+++ Exam Preparation III-IMDB/PHP Skeleton/src/AppBundle/Entity/Film.php
@@ -0,0 +1,151 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+/**
+ * Film
+ *
+ * @ORM\Table(name="films")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\FilmRepository")
+ */
+class Film
+{
+	//TODO: Implement me ...
+    /**
+     * @var int
+     *
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name",type="string", length=255)
+     * @NotBlank
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="genre", type="string",length=255)
+     * @NotBlank
+     */
+    private $genre;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="director", type="string",length=255)
+     * @NotBlank
+     */
+    private $director;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="year", type="integer")
+     * * @NotBlank
+     */
+    private $year;
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     * @return Film
+     */
+    public function setId(int $id): Film
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     * @return Film
+     */
+    public function setName( $name)
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getGenre()
+    {
+        return $this->genre;
+    }
+
+    /**
+     * @param string $genre
+     * @return Film
+     */
+    public function setGenre($genre)
+    {
+        $this->genre = $genre;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDirector()
+    {
+        return $this->director;
+    }
+
+    /**
+     * @param string $director
+     * @return Film
+     */
+    public function setDirector($director)
+    {
+        $this->director = $director;
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getYear()
+    {
+        return $this->year;
+    }
+
+    /**
+     * @param int $year
+     * @return Film
+     */
+    public function setYear($year)
+    {
+        $this->year = $year;
+        return $this;
+    }
+
+
+
+}
+
