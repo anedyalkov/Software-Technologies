fe4efb83710548de343d12ab0fd2bd68000e29dc
anedyalkov <nedyalkov78@gmail.com>
2017-08-27 18:04:28 +0300

    IMDB-C#

---
+++ Exam Preparation III-IMDB/C# Skeleton/IMDB/Controllers/FilmController.cs
@@ -0,0 +1,129 @@
+﻿using System.Linq;
+using System.Net;
+using System.Web.Mvc;
+using IMDB.Models;
+
+namespace IMDB.Controllers
+{
+    [ValidateInput(false)]
+    public class FilmController : Controller
+    {
+        [HttpGet]
+        [Route("")]
+        public ActionResult Index()
+        {
+            using (var db = new IMDBDbContext())
+            {
+                var films = db.Films.ToList();
+                return View(films);
+            }
+        }
+
+        [HttpGet]
+        [Route("create")]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("create")]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Film film)
+        {
+            if (ModelState.IsValid)
+            {
+                using (var db = new IMDBDbContext())
+                {
+                    db.Films.Add(film);
+                    db.SaveChanges();
+                    return Redirect("/");
+                }
+            }
+            return View();
+        }
+
+        [HttpGet]
+        [Route("edit/{id}")]
+        public ActionResult Edit(int? id)
+        {
+              using (var db = new IMDBDbContext())
+                {
+                    var movieForEdit = db.Films.Find(id);
+
+                    if (movieForEdit == null)
+                    {
+                        return HttpNotFound();
+                    }
+
+                    return View(movieForEdit);
+                }
+            }
+
+        [HttpPost]
+        [Route("edit/{id}")]
+        [ValidateAntiForgeryToken]
+        public ActionResult EditConfirm(int? id, Film filmModel)
+        {
+            if (ModelState.IsValid)
+            {
+                using (var db = new IMDBDbContext())
+                {
+                    var film = db.Films.Find(id);
+
+                    if (film == null)
+                    {
+                        return HttpNotFound();
+                    }
+
+                    film.Name = filmModel.Name;
+                    film.Genre = filmModel.Genre;
+                    film.Director = filmModel.Director;
+                    film.Year = filmModel.Year;
+
+                    db.SaveChanges();
+                }
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        [Route("delete/{id}")]
+        public ActionResult Delete(int? id)
+        {
+            using (var db = new IMDBDbContext())
+            {
+                var film = db.Films.Find(id);
+
+                if (film == null)
+                {
+                    return HttpNotFound();
+                }
+
+                return View(film);
+            }
+        }
+
+        [HttpPost]
+        [Route("delete/{id}")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirm(int? id, Film filmModel)
+        {
+            using (var db = new IMDBDbContext())
+            {
+                var film = db.Films.Find(id);
+
+                if (film == null)
+                {
+                    return HttpNotFound();
+                }
+
+                db.Films.Remove(film);
+                db.SaveChanges();
+            }
+
+            return RedirectToAction("Index");
+        }
+    }
+}
