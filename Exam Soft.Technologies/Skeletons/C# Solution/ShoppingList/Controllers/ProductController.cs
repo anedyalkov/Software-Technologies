3a8fef9a5346c1a48078e2cfc7dc6a664454a789
anedyalkov <nedyalkov78@gmail.com>
2017-10-27 09:43:04 +0300

    Shopping list-C#

---
+++ Exam Soft.Technologies/Skeletons/C# Solution/ShoppingList/Controllers/ProductController.cs
@@ -0,0 +1,90 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using ShoppingList.Models;
+
+namespace ShoppingList.Controllers
+{
+    [ValidateInput(false)]
+    public class ProductController : Controller
+    {
+        [HttpGet]
+        [Route("")]
+        public ActionResult Index()
+        {
+            using (var db = new ShoppingListDbContext())
+            {
+                var products = db.Products.ToList();
+                return View(products);
+            }
+        }
+
+        [HttpGet]
+        [Route("create")]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("create")]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                using (var db = new ShoppingListDbContext())
+                {
+                    db.Products.Add(product);
+                    db.SaveChanges();
+                    return Redirect("/");
+                }
+            }
+            return View();
+        }
+
+        [HttpGet]
+        [Route("edit/{id}")]
+        public ActionResult Edit(int? id)
+        {
+            using (var db = new ShoppingListDbContext())
+            {
+                var product = db.Products.Find(id);
+
+                if (product == null)
+                {
+                    return HttpNotFound();
+                }
+
+                return View(product);
+            }
+        }
+
+        [HttpPost]
+        [Route("edit/{id}")]
+        [ValidateAntiForgeryToken]
+        public ActionResult EditConfirm(int? id, Product productModel)
+        {
+            if (ModelState.IsValid)
+            {
+                using (var db = new ShoppingListDbContext())
+                {
+                    var product = db.Products.Find(id);
+
+                    if (product == null)
+                    {
+                        return HttpNotFound();
+                    }
+
+                    product.Priority = productModel.Priority;
+                    product.Name = productModel.Name;
+                    product.Quantity = productModel.Quantity;
+                    product.Status = productModel.Status;
+
+                    db.SaveChanges();
+                }
+            }
+
+            return RedirectToAction("Index");
+        }
+    }
+}
